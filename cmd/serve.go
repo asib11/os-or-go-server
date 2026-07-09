7444adab88a37a335c730b64a4ef9ef848361888
asib11 <ahmedasib1@gmail.com>
2026-07-09 13:58:17 +0600

    feat : refactor middleware

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,22 +2,20 @@ package cmd
 
 import (
 	"ecommerce/global_router"
-	"ecommerce/handlers"
 	"ecommerce/middleware"
 	"fmt"
 	"net/http"
 )
 
 func Serve() {
+	manager := middleware.NewManager()
+	manager.Use(middleware.Test, middleware.Logger) 
+
 	mux := http.NewServeMux() // Create a new ServeMux to handle routes. its called router in other languages
 
-	loggerMiddleware := middleware.Logger(http.HandlerFunc(handlers.TestHandler))
-	mux.Handle("GET /test", loggerMiddleware)
+	initRoutes(mux, manager)
 
-	mux.Handle("GET /products", http.HandlerFunc(handlers.GetProducts))
-	mux.Handle("POST /products", http.HandlerFunc(handlers.CreateProducts))
-	mux.Handle("GET /products/{id}", http.HandlerFunc(handlers.GetProductByID))
-
+	
 	globalRouter := global_router.GlobalRouter(mux)
 
 	fmt.Println("Server is running on http://localhost:8081")
