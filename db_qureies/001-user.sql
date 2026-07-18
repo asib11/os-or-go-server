3db746731da15ac073bb06a3af30e108dd58574d
asib11 <ahmedasib1@gmail.com>
2026-07-18 11:25:47 +0600

    feat #30 : data type of sql

--- db_qureies/001-user.sql
+++ db_qureies/001-user.sql
@@ -1,3 +1,26 @@
+
+-- database type: PostgreSQL
+-- SMALLINT 16 bits
+-- INT 32 bits
+-- BIGINT 64 bits
+-- SERIAL 32 bits auto-incrementing integer
+
+-- REAL 32 bits
+-- DOUBLE PRECISION 64 bits (best use for monetary values)
+-- NUMERIC variable precision
+
+
+-- CHAR(n) fixed length, blank padded
+-- VARCHAR(n) variable length
+-- TEXT variable unlimited length
+
+-- boolean true/false
+
+-- TIMESTAMP without time zone
+-- TIMESTAMPTZ with time zone
+
+
+
 CREATE TABLE users (
     id             SERIAL PRIMARY KEY,
     first_name     VARCHAR(100) NOT NULL,
@@ -8,3 +31,4 @@ CREATE TABLE users (
     created_at     TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at     TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
